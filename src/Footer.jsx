f7141e71bddce971b19b1c4a666dfe606d708fd5
Parthiban-sekar <dreamsparthi10222@gmail.com>
2024-03-23 15:10:09 +0530

    React-ToDo-Task

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+    
+    <footer class="footer fixed-bottom bg-dark text-light p-3">
+    <div class="container bg-dark ">
+      <p class="text-center bg-dark mb-0"><span className="bg-dark ">&copy; 2024 Parthiban Sekar. All rights reserved.</span></p>
+    </div>
+  </footer>
+
+    </>
+  );
+}
+
+export default Footer;
